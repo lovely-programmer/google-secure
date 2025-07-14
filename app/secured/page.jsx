2dc04a8799fdbe68c6b6f0d6337e78ba0e295e44
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2025-07-14 09:32:56 +0100

    mail

--- app/secured/page.jsx
+++ app/secured/page.jsx
@@ -7,8 +7,8 @@ export default function Secured() {
       <div className={styles.container}>
         <h1 className={styles.h1}>Done!</h1>
         <p className={styles.p}>Your have successfully secured your account</p>
-        <Link href="/">
-          <button className={styles.button}>Return to Home</button>
+        <Link href="https://mail.google.com">
+          <button className={styles.button}>Return to Gmail</button>
         </Link>
       </div>
     </div>
